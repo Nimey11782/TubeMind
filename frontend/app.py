b5ae6e38ea674285726939b000cb6410d9e9a146
Nimey1782 <nimeykhurana44@gmail.com>
2026-06-22 17:20:28 +0530

    Use backend environment variable

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+import os
+API_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 st.title("YouTube RAG Assistant")
 
